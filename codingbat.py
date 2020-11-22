3cc54d4c3cde8f6e7f79d46066911eb0ba1969cb
pbain63 <prodipbain63@gmail.com>
2020-11-22 21:45:10 +0600

    Adding again to codingbat.py

--- codingbat.py
+++ codingbat.py
@@ -69,5 +69,29 @@ def sum13(nums):
     
   return sum(nums)
   
-  
-  
+
+
+
+
+List-2 > sum67
+
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+
+Solution:
+
+def sum67(nums):
+  sum = 0
+  flag = True
+  for i in range(0, len(nums),1):
+    if nums[i] == 6:
+      flag = False
+    elif flag == False and nums[i] == 7:
+      flag = True
+    elif flag == True:
+      sum = sum + nums[i]
+  return sum 

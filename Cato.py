9b0e8084e2b66424f829b0d49d9a959552100145
pbain63 <prodipbain63@gmail.com>
2020-11-30 22:11:09 +0600

    Adding for fibbobacci to Cato.py

--- Cato.py
+++ Cato.py
@@ -41,3 +41,33 @@ for num in numbers:
 
     # Print `nth`
     print(nth)
+
+### Fibonacci series by while loop
+
+# Set `fib_no` to 55, the number until where you want to print
+fib_no = 55
+
+# Set `first_no` to 0
+first_no = 0
+
+# Set `second_no` to 1
+second_no = 1
+
+# Set the counter `count` to 0
+count = 0
+
+# while loop to print the fibonacci series until the `fib_no`
+while first_no <= fib_no:
+    # Print `first_no`
+    print(first_no)
+
+    # Fibonnacci number
+    nth = first_no + second_no
+
+    # update values of `first_no` and `second_no`
+    first_no = second_no
+    second_no = nth
+
+    # Set counter `count` +1
+    count += 1
+

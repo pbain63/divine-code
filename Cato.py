86248074054a0ead1de5fc69c114762cd79e193f
pbain63 <prodipbain63@gmail.com>
2020-11-30 22:07:47 +0600

    Adding new program to Cato.py

---
+++ Cato.py
@@ -0,0 +1,16 @@
+### nested pattern
+
+# Initialize the first five rows
+n = 5
+
+# Start the loop to print the first five rows
+for i in range(n):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+
+# Start the loop to print the remaining rows in decreasing order of stars
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')

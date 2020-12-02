149c4df454bd9a5572fc77406ef52808bca398a7
pbain63 <prodipbain63@gmail.com>
2020-12-02 22:14:29 +0600

    Managing some code to Cato.py

--- Cato.py
+++ Cato.py
@@ -176,18 +176,5 @@ while True:
     print("Smallest Fibonacci Number larger than x: " + str(sup))
 
 
-##
-###Program: Function recursion
-
-def tri_recursion(k):
-  if(k > 0):
-    result = k + tri_recursion(k - 1)
-    print(result)
-  else:
-    result = 0
-  return result
-
-print("\n\nRecursion Example Results")
-tri_recursion(6)
 
 ##

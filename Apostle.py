a5680e36ebf81427f4807425f560f8bc012005ac
pbain63 <prodipbain63@gmail.com>
2020-12-03 20:20:10 +0600

    Adding swap code to Apostle.py

---
+++ Apostle.py
@@ -0,0 +1,21 @@
+### Simple coding problems
+
+
+## Swap two variables1
+
+(x,y) = (10,50)
+(x,y) = (y,x)
+print(x,y)
+
+
+## Swap two variables2
+x = 5
+y = 10
+
+
+temp = x
+x = y
+y = temp
+
+print("The value of x after swapping: ", x)
+print("The value of y after swapping: ", y)

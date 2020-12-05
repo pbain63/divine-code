7dd178cb8769afbf47ceed6bdcf9dcf39e2c5657
pbain63 <prodipbain63@gmail.com>
2020-12-05 17:15:25 +0600

    Adding random program to Thomas.py

--- Thomas.py
+++ Thomas.py
@@ -219,3 +219,16 @@ def sum_digits3(n):
        r, n = r + n % 10, n // 10
    return r
 print(sum_digits3(123))
+
+###
+
+###generate a random number:
+
+import random
+n = random.randint(2, 10)
+print(n)
+
+##
+import random
+print(random.random())
+

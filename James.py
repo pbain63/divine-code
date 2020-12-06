95f15c7a1359d919d5f3cbacf0bcf11d909d7bf7
pbain63 <prodipbain63@gmail.com>
2020-12-06 23:23:10 +0600

    Adding to James.py

--- James.py
+++ James.py
@@ -57,3 +57,19 @@ else:
     print("Not Prime.")
 
 ###
+
+###Remove duplicate from a list
+
+mylist = ["a", "b", "a", "c", "c"]
+mylist = list(dict.fromkeys(mylist))
+print(mylist)
+
+###
+def my_function(x):
+  return list(dict.fromkeys(x))
+
+mylist = my_function(["a", "b", "a", "c", "c"])
+
+print(mylist)
+
+
